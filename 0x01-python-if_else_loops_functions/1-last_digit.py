b502578df0dff85400ae0c7de0bf127059cd6f2b
samwelopondo <opondosamwel2@gmail.com>
2022-12-01 10:16:42 +0300

    High level

---
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+num = str(number)
+print(number)
+print(num[-1])
+
+if int(num[-1]) > 5:
+    var = "and is greater than 5"
+
+elif int(num[-1]) == 0:
+    var = "and is 0"
+
+elif int(num[-1]) < 6 and not 0:
+    var = "and is less than 6 and not 0"
+
+print(f"Last digit of {number} is {num[-1]} {var}")
