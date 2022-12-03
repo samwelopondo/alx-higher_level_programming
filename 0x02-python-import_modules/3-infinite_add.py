6fcac50b950778335cf2afb3f9b2158bba34830b
samwelopondo <opondosamwel2@gmail.com>
2022-12-03 16:29:19 +0300

    0

--- 0x02-python-import_modules/3-infinite_add.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-import sys
-
-if __name__ == "__main__":
-    args = sys.argv
-    sum = 0
-    for i in range(1, len(args)):
-        sum += int(args[i])
-    print("{:d}".format(sum))
