07602d21fe151f0d64529cda587d9aac124b5924
samwelopondo <opondosamwel2@gmail.com>
2022-12-05 12:25:38 +0300

    0

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 
 def multiple_returns(sentence):
-    length = len(sentence)
-
-    if len(sentence) == 0:
+    if not sentence:
         first = None
 
     first = sentence[0]
+    length = len(sentence)
 
     result = (length, first)
     return result
