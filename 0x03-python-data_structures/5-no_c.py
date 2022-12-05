01013bae11eeb9716cd9bf2f7bd1ea6b994e45b6
samwelopondo <opondosamwel2@gmail.com>
2022-12-05 09:27:09 +0300

    0

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 
 def no_c(my_string):
-    my_string = list(my_string)
-    for i in range(len(my_string)):
-        if my_string[i] == 'c':
-            my_string[i] = ' '
-    _str = ""
-    for element in my_string:
-        _str += element
-    return _str
+    if my_string: 
+        my_string = list(my_string)
+        for i in range(len(my_string)):
+            if my_string[i] == 'c' or my_string[i] == 'C':
+                my_string[i] = ' '
+        my_str = ""
+        for element in my_string:
+            my_str += element
+        return my_str
