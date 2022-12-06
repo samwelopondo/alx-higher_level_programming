7fc1db0c4e4c0abf0347a5f680e13e5b100bbc83
samwelopondo <opondosamwel2@gmail.com>
2022-12-06 10:37:28 +0300

    0

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if my_list is None:
+        return None
+
+    for i in range(len(my_list)):
+        for j in range(len(my_list) - 1):
+            if my_list[j] > my_list[j+1]:
+                temp = my_list[j]
+                my_list[j] = my_list[j+1]
+                my_list[j+1] = temp
+    return (my_list[len(my_list) - 1])
