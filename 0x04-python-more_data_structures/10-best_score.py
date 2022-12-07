8ea233d0aa4e4a92bbb8c7fd31bb0801e9658de6
samwelopondo <opondosamwel2@gmail.com>
2022-12-07 18:24:54 +0300

    1

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -4,6 +4,9 @@ def best_score(a_dictionary):
     if a_dictionary is None:
         return None
 
+    if a_dictionary == {}:
+        return None
+
     keys = list(a_dictionary.keys())
     values = list(a_dictionary.values())
 
