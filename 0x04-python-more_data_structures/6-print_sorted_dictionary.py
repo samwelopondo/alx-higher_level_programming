63e80334061f3ef9f69bb82f8868efc4093a75d6
samwelopondo <opondosamwel2@gmail.com>
2022-12-07 16:42:55 +0300

    0

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -11,7 +11,9 @@ def print_sorted_dictionary(a_dictionary):
         items = list(a_dictionary.items())
 
         for i in range(len(lisp)):
-            result[lisp[i]] = items[i][1]
+            for j in range(len(items)):
+                if lisp[i] == items[j][0]:
+                    result[lisp[i]] = items[j][1]
 
         for k, v in result.items():
             print("{:s} {}".format(k, v))
